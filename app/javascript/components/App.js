d81d292da333df0323cbb1bfa2fb7ce644970c67
enuii3 <b.nxxxi551@gmail.com>
2020-11-10 16:10:59 +0900

    react-routerの導入

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,13 +1,25 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
 
 import PortfolioCard from './PortfolioCard'
+import UserCard from './UserCard'
 
 export default function App(){
   return(
-    <>
-      <PortfolioCard />
-    </>
+    <Router>
+      <Switch>
+        <Route exact path={'/'}>
+          <PortfolioCard />
+        </Route>
+        <Route path={'/users/:id'}>
+          <UserCard />
+        </Route>
+      </Switch>
+    </Router>
   )
 }
-
 
