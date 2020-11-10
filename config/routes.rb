c45194eea32c691410d05493e90cd7282889d89d
enuii3 <b.nxxxi551@gmail.com>
2020-11-10 18:36:33 +0900

    feat: UserCardコンポーネントを作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
   root 'home#index'
   devise_for :users
 
+  resources 'users', to: 'home#index'
+  resources :portfolios, to: 'home#index'
+
   namespace :api, { format: 'json' } do
     namespace :v1 do
+      resources :users, only: [:show]
       resources :portfolios, only: [:index]
     end
   end
