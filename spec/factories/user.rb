004c5c9c634e46ef133fa542cada035a3aa3b10f
enuii3 <b.nxxxi551@gmail.com>
2020-11-10 16:13:52 +0900

    fix: UserモデルにselfIntroductionカラムを追加

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -3,5 +3,6 @@ FactoryBot.define do
     name { Faker::Name.name }
     sequence(:email) { |n| "sample#{n}@portfolder.com" }
     encrypted_password { 'hogehoge' }
+    self_introduction { Faker::Lorem.paragraph_by_chars }
   end
 end
